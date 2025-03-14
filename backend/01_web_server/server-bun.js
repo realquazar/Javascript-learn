26782e3e75d53c24563e28540a184e2a61f88cbb
Quazar <112936536+realquazar@users.noreply.github.com>
2025-03-14 14:34:43 +0530

    Create server-bun.js

---
+++ backend/01_web_server/server-bun.js
@@ -0,0 +1,17 @@
+import {serve} from "bun";
+serve({
+    fetch(request) {
+        const url = new URL(request.url);
+        if(url.pathname === "/") {
+            return new Response("Server is running", {status: 200})
+        }
+        else if(url.pathname === "/coffee") {
+            return new Response("Coffee is good", {status: 200})
+        }
+        else {
+            return new Response("Eror 404", {status: 404})
+        }
+    },
+    port: 3000,
+    hostname: "127.0.0.1"
+})
